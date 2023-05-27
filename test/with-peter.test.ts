79fe5c464b8b52e94627759e18aeef1be09dd1e2
focusotter <mtliendo@focusotter.com>
2023-05-27 11:44:31 -0500

    Initial commit

---
+++ test/with-peter.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WithPeter from '../lib/with-peter-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/with-peter-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WithPeter.WithPeterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
